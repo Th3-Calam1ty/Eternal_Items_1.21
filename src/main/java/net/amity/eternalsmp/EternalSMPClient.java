c81f2b469fc8f9d641a4b7d83e538b82f215127d
amity <amitynoll@gmail.com>
2025-02-15 16:13:19 -0600

    Initial commit

---
+++ src/main/java/net/amity/eternalsmp/EternalSMPClient.java
@@ -0,0 +1,10 @@
+package net.amity.eternalsmp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EternalSMPClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/amity/eternalsmp/EternalSMPItems.java
@@ -0,0 +1,16 @@
+package net.amity.eternalsmp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EternalSMPItems implements ModInitializer {
+	public static final String MOD_ID = "eternalsmp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
